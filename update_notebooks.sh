1fcf362a57431dd271ee80f3f0a54c5ab148c110
Doug Williams <ddwilli@gmail.com>
2015-03-24 09:52:26 -0400

    Updated notebooks

---
+++ update_notebooks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# see: https://github.com/paulgb/runipy/
+#runipy -o 'Curves_DecisionTree.ipynb'
+#runipy -o 'Curves_SGD.ipynb'
+#runipy -o 'Curves_ExtraTree.ipynb'
+#runipy -o 'Curves_SVM.ipynb'
+#runipy -o 'Curves_LogisticRegression.ipynb'
+#runipy -o 'Curves_AdaBoost.ipynb'
+#runipy -o 'Curves_NaiveBayes.ipynb'
+#runipy -o 'Curves_GradientTreeBoosting.ipynb'
+#runipy -o 'Curves_RandomForest.ipynb'
+runipy -o 'Composite_learner.ipynb'
